8b9bdb5c88eee41b8c458e906d2c1a6a547a038c
Jeevan Kumar Papagari <48345440+jeevankumarpapagari@users.noreply.github.com>
2021-12-28 15:02:25 +0530

    T 14 - Printing an Array in Reverse

---
+++ T 14 - Printing an Array in Reverse.py
@@ -0,0 +1,10 @@
+l = [int(x) for x in input().split()]
+for i in range(-1,-len(l)-1,-1):
+    print(l[i],end=" ")
+    
+#OR
+
+l = [int(x) for x in input().split()]
+l = l[::-1]
+for i in l:
+    print(i,end=" ")
